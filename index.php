4eb73c40b0f7c22c88ec509a1e37b73595879e7a
gadeshivani <gadeshivani@gmail.com>
2017-10-16 03:45:03 -0400

    modified index.php

--- index.php
+++ index.php
@@ -4,13 +4,44 @@
 require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
 use Twilio\Twiml;
 
+
+
 $response = new Twiml;
 $body = $_REQUEST['Body'];
+function object_to_array($data)
+{
+    if (is_array($data) || is_object($data))
+    {
+        $result = array();
+        foreach ($data as $key => $value)
+        {
+            $result[$key] = object_to_array($value);
+        }
+        return $result;
+    }
+    return $data;
+}
 
+function getSynonims($body)
+{
+    $url = 'https://www.googleapis.com/scribe/v1/research?key=AIzaSyDqVYORLCUXxSv7zneerIgC2UYMnxvPeqQ&dataset=dictionary&dictionaryLanguage=en&query='.$body;
+    $ret = null;
+    $data = file_get_contents($url);
+    $data = object_to_array(json_decode($data));
+    if (isset($data['data'][0]['dictionary']['definitionData']['0']['meanings'][0]['synonyms']))
+        $synonyms = $data['data'][0]['dictionary']['definitionData']['0']['meanings'][0]['synonyms'];
+    foreach ($synonyms as $key => $synonym) {
+        $ret[$key] = $synonym['nym'];
+    }
+    return $ret;
+}
 if( $body == 'hello' ){
     $response->message('Hi!');
 }else if( $body == 'bye' ){
     $response->message('Goodbye');
+}else{
+	
+	getSynonims($body);
 }
 print $response;
 
