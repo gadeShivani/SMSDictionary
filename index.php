4bf45fd93540077f14e1373798d8cb2cc4dd5051
chrisz24 <chriszikopoulos@gmail.com>
2017-10-17 13:13:00 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,23 @@
 <?php
 // Get the PHP helper library from twilio.com/docs/php/install
-
 require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
 use Twilio\Twiml;
-
-
-
 $response = new Twiml;
-$key = $_REQUEST['Body'];
-
+$body = $_REQUEST['Body'];
 $json = dictionary.json;
 
+JSON;
 $jsonIterator = new RecursiveIteratorIterator(
     new RecursiveArrayIterator(json_decode($json, TRUE)),
     RecursiveIteratorIterator::SELF_FIRST);
-
 foreach ($jsonIterator as $key => $val) {
-    if(is_array($val)) {
-        echo "$key:\n";
-    } else {
-        echo "$key => $val\n";
-    }
+   if( $body == $key ){
+	  
+    $response->$val;
+	
+}else if( $body == 'bye' ){
+    $response->message('Goodbye');
 }
-
-
+}
+print $response;
 ?>
