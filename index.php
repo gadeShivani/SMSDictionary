f013e6dde6f53653ea8936068bee05b5b5885358
chrisz24 <chriszikopoulos@gmail.com>
2017-10-17 15:29:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,32 @@ if( $body == 'Hello' ){
 }else if( $body == 'bye' ){	
     $response->message('Goodbye');
 }
+else if( $body == 'Obtain' ){	
+    $response->message('Obtain: Come into possession of');
+}
+else if( $body == 'Scarce' ){	
+    $response->message('Scarce: deficient in quantity or number compared with the demand');
+}
+else if( $body == 'Policy' ){	
+    $response->message('Policy: a plan of action adopted by an individual or social group');
+}
+else if( $body == 'Straight' ){	
+    $response->message('Straight: successive, without a break');
+}
 else if( $body == 'Concept' ){	
     $response->message('Concept: an abstract or general idea inferred from specific instances');
+}
+else if( $body == 'Stock' ){	
+    $response->message('Stock: capital raised by a corporation through the issue of shares');
+}
+else if( $body == 'Apparent' ){	
+    $response->message('Apparent: clearly revealed to the mind or the senses or judgment');
+}
+else if( $body == 'Property' ){	
+    $response->message('Property: a basic or essential attribute shared by members of a class');
+}
+else if( $body == 'Fancy' ){	
+    $response->message('Fancy: imagine; conceive of; see in ones mind');
 }
 else{
 	
