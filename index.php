5a6c9b6af355364d3aa6c0b84ae9827b5db51b3f
chrisz24 <chriszikopoulos@gmail.com>
2017-10-17 15:25:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,27 +4,7 @@ require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
 use Twilio\Twiml;
 $response = new Twiml;
 $body = $_REQUEST['Body'];
-function object_to_array($data)
-{
-    if (is_array($data) || is_object($data))
-    {
-        $result = array();
-        foreach ($data as $key => $value)
-        {
-            $result[$key] = object_to_array($value);
-        }
-        return $result;
-    }
-    return $data;
-}
-function getSynonims($body)
-{
-    $url = 'http://googledictionary.freecollocation.com/meaning?word='.$body;
-    
-    $data = file_get_contents($url);
-    
-    return $data;
-}
+
 if( $body == 'Hello' ){
     $response->message('Hi!');
 }else if( $body == 'bye' ){	
