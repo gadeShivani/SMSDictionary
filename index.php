6fd42d0b7f233dd67c380802f2f45b356df3029b
gadeshivani <gadeshivani@gmail.com>
2017-10-18 03:39:40 -0400

    changed and fixed

--- index.php
+++ index.php
@@ -7,12 +7,21 @@ use Twilio\Twiml;
 
 
 $response = new Twiml;
-$body = $_REQUEST['Body'];
+$body = $_REQUEST['keyword'];
 
 // $json = './dictionary.json';
-// $jsonIterator = new RecursiveIteratorIterator(
-//     new RecursiveArrayIterator(json_decode($json, TRUE)),
-//     RecursiveIteratorIterator::SELF_FIRST);
+$json = <<< JSON
+{
+    "John":56,
+    "Jennifer":45,
+    "James":44
+}
+JSON;
+
+$jsonIterator = new RecursiveIteratorIterator(
+    new RecursiveArrayIterator(json_decode($json, TRUE)),
+    RecursiveIteratorIterator::SELF_FIRST);
+
 
 foreach ($jsonIterator as $key => $val) {
    if( $body == $key ){
