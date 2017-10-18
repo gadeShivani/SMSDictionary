2d89352fb1d3c98aef2b864b2fb5da07ebcb8266
gadeshivani <gadeshivani@gmail.com>
2017-10-18 03:16:00 -0400

    edited var_dump

---
+++ Desktop/TwilioEducation-DictionaryTrial/index.php
@@ -0,0 +1,26 @@
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+
+require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
+use 6to Twilio\Twiml;
+
+
+
+$response = new Twiml;
+$body = $_REQUEST['Body'];
+
+$json = './dictionary.json';
+$jsonIterator = new RecursiveIteratorIterator(
+    new RecursiveArrayIterator(json_decode($json, TRUE)),
+    RecursiveIteratorIterator::SELF_FIRST);
+
+foreach ($jsonIterator as $key => $val) {
+   if( $body == $key ){
+	 var_dump($val);
+	 die();
+    $response->message($val);
+
+}
+print $response;
+
+?>
