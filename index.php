fb06813040d97a64485eca6db01744ffc669fafa
gadeshivani <gadeshivani@gmail.com>
2017-10-18 13:40:01 -0400

    debiugging response #3

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $jsonIterator = new RecursiveIteratorIterator(
     RecursiveIteratorIterator::SELF_FIRST);
 
   $meaning = $obj['body']['list'];
-    echo $meaning;
+    echo $meaning[0];
 foreach ($jsonIterator as $key => $val) {
 print_r($key);
 print_r($val);
