2db0d6bbe29d299b291d5001786d8792abfc7760
gadeshivani <gadeshivani@gmail.com>
2017-10-18 11:25:58 -0400

    modified index.php

--- index.php
+++ index.php
@@ -33,8 +33,8 @@ $jsonIterator = new RecursiveIteratorIterator(
     );
    //print_r($urban_response);
   $obj=json_encode($urban_response,TRUE);
-    $meaning = $obj['list'][5]['defination'];
-    print_r($meaning);
+    //$meaning = $obj['list'][5]['defination'];
+    print_r($obj);
     echo $meaning;
 foreach ($jsonIterator as $key => $val) {
 
